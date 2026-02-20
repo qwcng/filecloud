17cccbc4cb8309ab129f831a04d89fb827bc7836
qwcng <Pogchampik132@interia.pl>
2026-02-20 17:13:17 +0100

    PWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -49,16 +49,16 @@
                     <meta property="og:url" content="https://filecloud.ct8.pl" />
                     <meta property="og:title" content="Versec Drive - Keep your files safe and private" />
                     <meta property="og:description" content="Experience the ultimate cloud storage solution with Versec Drive. Store your files securely, share them effortlessly, and collaborate seamlessly with your team." />
-                    <meta property="og:image" content="https://filecloud.ct8.pl/versec.jpg" />
+                    <meta property="og:image" content="https://filecloud.ct8.pl/logo.png" />
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:url" content="https://filecloud.ct8.pl" />
                     <meta property="twitter:title" content="Versec Drive - Keep your files safe and private" />
                     <meta property="twitter:description" content="Experience the ultimate cloud storage solution with Versec Drive. Store your files securely, share them effortlessly, and collaborate seamlessly with your team." />
-                    <meta property="twitter:image" content="https://filecloud.ct8.pl/versec.jpg" />
+                    <meta property="twitter:image" content="https://filecloud.ct8.pl/logo.png" />
 
         <link rel="icon" href="/icon.ico" sizes="any">
         {{-- <link rel="icon" href="/icon.svg" type="image/svg+xml"> --}}
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/logo.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
